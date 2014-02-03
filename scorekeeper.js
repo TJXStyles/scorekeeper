a41d623e146b8c076b15a71454dc8755792d6dfa
Jack Cooc <jack.cooc@gmail.com>
2014-02-03 16:49:57 -0500

    leaderboard template

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,19 +1,96 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Meteor.Collection("players");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to scorekeeper.";
+
+  Session.setDefault("sort", {score: -1, name: 1});
+
+  Template.leaderboard.players = function () {
+    return Players.find({}, {sort: Session.get("sort")});
+  };
+  /* Descirbes in English how the list is sorted */
+  Template.leaderboard.sort = function () {
+    var sort = Session.get("sort");
+    var output = "Sorting by ";
+    _.each(sort, function(v,k,l) {
+      var order  = "";
+      if (v === 1)
+        order = "ascending";
+      if (v === -1)
+        order = "descending";
+      output += k+" "+order+", ";
+    });
+    return output;
+  };
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "warning" : '';
+  };
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  /* When you press the button, add 3 points to the player */
+  Template.leaderboard.events({
+    'click #increment3': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 3}});
+    },
+    'click #increment1': function () {
+    Players.update(Session.get("selected_player"), {$inc: {score: 1}});
+    },
+    'click #delete': function () {
+      Players.remove(Session.get("selected_player"));
+    },
+    'click #insert': function () {
+      var n = $("input[name=name]").val();
+      if (n != "") {
+        Players.insert({name: n, score: 0});
+        $("input[name=name]").val('');
+      }
+      // return false so the page will not reload
+      return false;
+    },
+    'click #name': function () {
+      var sort = Session.get("sort");
+      sort = {"name" : sort.name * -1, score: sort.score }
+
+      Session.set("sort", sort);
+    },
+    'click #score': function () {
+      var sort = Session.get("sort");
+      if (_.has(sort,"score"))
+        sort = {score: sort.score * -1, name: sort.name }
+      else
+        sort = {score: -1, name: sort.name}
+
+      Session.set("sort", sort);
+    },
+    'click #shuffle': function () {
+      // get all players
+      var players = Players.find({}).fetch();
+
+      // clear all players
+      for (var i = 0; i < players.length; i++)
+      {
+        Players.remove(players[i]._id);
+      }
+
+      // Re-insert players with new scores
+      for (var i = 0; i < players.length; i++)
+      {
+        var player = {name: players[i].name, score: 0};
+        Players.insert(player);
+      }
+    },
+  });
+
+  /* When you click a player, select it */
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+    }
   });
 }
