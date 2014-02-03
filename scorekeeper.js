b13ad0e6e30ef2d1fe2fcc8c5489419b115169e9
Jack Cooc <jack.cooc@gmail.com>
2014-02-03 17:13:14 -0500

    add increment by 3 button

--- scorekeeper.js
+++ scorekeeper.js
@@ -4,47 +4,30 @@
 Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
-
-  Session.setDefault("sort", {score: -1, name: 1});
-
   Template.leaderboard.players = function () {
-    return Players.find({}, {sort: Session.get("sort")});
+    return Players.find({}, {sort: {score: -1, name: 1}});
   };
-  /* Descirbes in English how the list is sorted */
-  Template.leaderboard.sort = function () {
-    var sort = Session.get("sort");
-    var output = "Sorting by ";
-    _.each(sort, function(v,k,l) {
-      var order  = "";
-      if (v === 1)
-        order = "ascending";
-      if (v === -1)
-        order = "descending";
-      output += k+" "+order+", ";
-    });
-    return output;
-  };
+
   Template.leaderboard.selected_name = function () {
     var player = Players.findOne(Session.get("selected_player"));
     return player && player.name;
   };
 
   Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "warning" : '';
+    return Session.equals("selected_player", this._id) ? "selected" : '';
   };
 
-  /* When you press the button, add 3 points to the player */
   Template.leaderboard.events({
-    'click #increment3': function () {
+    'click input.inc3': function () {
       Players.update(Session.get("selected_player"), {$inc: {score: 3}});
     },
-    'click #increment1': function () {
+    'click input.inc1': function () {
     Players.update(Session.get("selected_player"), {$inc: {score: 1}});
     },
-    'click #delete': function () {
-      Players.remove(Session.get("selected_player"));
+    'click input.reset': function () {
+    Players.update(Session.get("selected_player"), {score: 0});
     },
-    'click #insert': function () {
+      'click #insert': function () {
       var n = $("input[name=name]").val();
       if (n != "") {
         Players.insert({name: n, score: 0});
@@ -53,41 +36,8 @@ if (Meteor.isClient) {
       // return false so the page will not reload
       return false;
     },
-    'click #name': function () {
-      var sort = Session.get("sort");
-      sort = {"name" : sort.name * -1, score: sort.score }
-
-      Session.set("sort", sort);
-    },
-    'click #score': function () {
-      var sort = Session.get("sort");
-      if (_.has(sort,"score"))
-        sort = {score: sort.score * -1, name: sort.name }
-      else
-        sort = {score: -1, name: sort.name}
-
-      Session.set("sort", sort);
-    },
-    'click #shuffle': function () {
-      // get all players
-      var players = Players.find({}).fetch();
-
-      // clear all players
-      for (var i = 0; i < players.length; i++)
-      {
-        Players.remove(players[i]._id);
-      }
-
-      // Re-insert players with new scores
-      for (var i = 0; i < players.length; i++)
-      {
-        var player = {name: players[i].name, score: 0};
-        Players.insert(player);
-      }
-    },
   });
 
-  /* When you click a player, select it */
   Template.player.events({
     'click': function () {
       Session.set("selected_player", this._id);
